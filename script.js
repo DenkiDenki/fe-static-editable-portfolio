2b78e5d6179495f6ed884c0b555e133502a72bd0
Denisa <denisa.gerez@gmail.com>
2022-09-29 00:10:16 -0300

    add scroll and snaps-inline CSS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const element = document.querySelector(".container-content");
+const element = document.querySelector(".container-scroller");
 
 element.addEventListener("wheel", (event) => {
   element.scrollBy({
     left: event.deltaY < 0 ? -800 : 800,
     behavior: "smooth",
   });
-  console.log(event);
+  //console.log(event);
 });
