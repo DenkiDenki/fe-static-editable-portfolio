04f98fd547b4acb67b17850adb234fda08b80aa2
Denisa <denisa.gerez@gmail.com>
2022-10-01 21:23:29 -0300

    se agrego un boton de edición para el titulo principal y un campo de input

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-const element = document.querySelector(".container-scroller");
+const d = document;
+const $element = d.querySelector(".container-scroller");
 
-element.addEventListener("wheel", (event) => {
-  element.scrollBy({
+$element.addEventListener("wheel", (event) => {
+  $element.scrollBy({
     left: event.deltaY < 0 ? -800 : 800,
     behavior: "smooth",
   });
   //console.log(event);
 });
+/*********** */
+const $editButton = d.querySelector("#edit-btn");
+
+//visualizar el boton editButton y el input de texto
+//tomar el input de texto, hacerlo visible y permitir editarlo
+// guardar ese texto en el title
+//volver a ocultar el botón
+function editContent() {
+  d.getElementById("edit-title").style.display = "block";
+  let heroTitle = d.getElementById("hero-title").innerText;
+  console.log(heroTitle);
+}
+
+function setHeroTitle(valor) {
+  d.getElementById("hero-title").innerText = valor;
+}
+
+function logMessage(message) {
+  console.log(message + "<br>");
+}
+
+//controla si se preciona enter
+let textarea = d.getElementById("edit-title");
+textarea.addEventListener("keyup", (e) => {
+  logMessage(`Key "${e.key}" released [event: keyup]`);
+
+  if (e.key == "Enter") {
+    d.getElementById("edit-title").style.display = "none";
+  }
+});
+
+$editButton.addEventListener("click", editContent);
